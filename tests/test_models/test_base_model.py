e475fc579bc5d83a73f5d06a783fd87b36635eaf
QueenMaggie <wanjirumargaret394@gmail.com>
2023-11-12 05:53:21 -0800

    test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,7 +11,9 @@ from models.base_model import BaseModel
 
 
 class TestBaseModel_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the BaseModel class."""
+    """
+    Unittests for testing instantiation of the BaseModel class.
+    """
 
     def test_no_args_instantiates(self):
         self.assertEqual(BaseModel, type(BaseModel()))
