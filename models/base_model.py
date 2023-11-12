452b4f76a6f0e85bd4055f1f6333116732c1fb3e
QueenMaggie <wanjirumargaret394@gmail.com>
2023-11-12 07:24:17 -0800

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,7 @@ from datetime import datetime
 class BaseModel:
     """Represents the BaseModel of the HBnB project."""
 
-    def __init__(self, args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel.
         Args:
             *args (any): Unused.
