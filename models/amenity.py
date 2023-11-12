d41b2c7131945de60dabf81724816f2e59bf40d7
QueenMaggie <wanjirumargaret394@gmail.com>
2023-11-12 07:26:12 -0800

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -7,6 +7,8 @@ from models.base_model import BaseModel
 
 class Amenity(BaseModel):
 
-    """Class for managing amenity objects"""
+    """
+    Class for managing amenity objects
+    """
 
     name = ""
