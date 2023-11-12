e119fd98e252e522069873959ee6c8803ad010a0
QueenMaggie <wanjirumargaret394@gmail.com>
2023-11-12 05:23:02 -0800

    state.py

--- models/state.py
+++ models/state.py
@@ -7,8 +7,6 @@ from models.base_model import BaseModel
 
 class State(BaseModel):
 
-    """
-    Class for managing state objects
-    """
+    """Class for managing state objects"""
 
     name = ""
