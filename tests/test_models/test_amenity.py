e86148a2649cc0c70768afb1647e0c797e973f92
QueenMaggie <wanjirumargaret394@gmail.com>
2023-11-12 05:47:19 -0800

    test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -16,7 +16,9 @@ from models.amenity import Amenity
 
 
 class TestAmenity_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the Amenity class."""
+    """
+    Unittests for testing instantiation of the Amenity class.
+    """
 
     def test_no_args_instantiates(self):
         self.assertEqual(Amenity, type(Amenity()))
