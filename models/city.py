3dbc29af3a2e7267484e25594a159f843231c06b
QueenMaggie <wanjirumargaret394@gmail.com>
2023-11-12 05:05:54 -0800

    city.py

--- models/city.py
+++ models/city.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+"""#!/usr/bin/python3
 """This module creates a City class"""
 
 
@@ -6,8 +6,6 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """
-    Class for managing city objects
-    """
+   """ Class for managing city objects"""
     state_id = ""
     name = ""
